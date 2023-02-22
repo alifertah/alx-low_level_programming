f5869e1ff85bace9221dfad1e4e5b7bdbf08d3f4
alifertah <alifertah42@gmail.com>
2023-02-22 02:44:27 +0100

    exercise 03

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - int _islower
+ * Description: Pritn alphebet in lower case
+ * Return: void
+ * @c: int c
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
